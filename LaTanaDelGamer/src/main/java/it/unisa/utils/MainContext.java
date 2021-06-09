3921474f0f84efbef68ec7c958822de9535df40e
rbalestrieri5 <r.balestrieri5@studenti.unisa.it>
2021-06-09 10:15:28 +0200

    MainContext adding

--- LaTanaDelGamer/src/main/java/it/unisa/utils/MainContext.java
+++ LaTanaDelGamer/src/main/java/it/unisa/utils/MainContext.java
@@ -1,8 +1,13 @@
 package it.unisa.utils;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 @WebListener
 public class MainContext implements ServletContextListener {
@@ -10,6 +15,18 @@ public class MainContext implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		Utility.print("Startup web application");
+		
+		ServletContext context = sce.getServletContext();
+		
+		DataSource ds = null;
+		try {
+		Context initCtx = new InitialContext();
+		Context envCtx = (Context) initCtx.lookup("java.comp/env");
+		
+		ds = (DataSource) envCtx.lookup("jdbc/la_tana_del_gamer");
+		} catch(NamingException e) {
+			Utility.print(e);
+		}
 	}
 	
 	@Override
