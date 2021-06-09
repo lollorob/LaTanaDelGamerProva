a63e4beab604b4d057007bdaaddf2b64e970e9b8
rbalestrieri5 <r.balestrieri5@studenti.unisa.it>
2021-06-09 15:57:30 +0200

    accountBean e prodottoBean

--- LaTanaDelGamer/src/main/java/it/unisa/model/AccountUserBean.java
+++ LaTanaDelGamer/src/main/java/it/unisa/model/AccountUserBean.java
@@ -4,4 +4,109 @@ import java.io.Serializable;
 
 public class AccountUserBean implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	String username;
+    String eMail;
+    String passwd;
+    String nome;
+    String cognome;
+    String data;
+    int n_Ordini;
+    String via;
+    int numero;
+    long cap;
+    String citta;
+    String provincia;
+    
+    
+    public AccountUserBean() {
+    	
+    	username="";
+        eMail="";
+        passwd="";
+        nome="";
+        cognome="";
+        data="NNNN--NN-NN";
+        n_Ordini=-1;
+        via="";
+        numero=-1;
+        cap=-1;
+        citta="";
+        provincia="";
+    }
+    
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public int getn_Ordini() {
+		return n_Ordini;
+	}
+	public void setn_Ordini(int nOrdini) {
+		this.n_Ordini = nOrdini;
+	}
+	public String getVia() {
+		return via;
+	}
+	public void setVia(String via) {
+		this.via = via;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public long getCap() {
+		return cap;
+	}
+	public void setCap(long cap) {
+		this.cap = cap;
+	}
+	public String getCitta() {
+		return citta;
+	}
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+    	
 }
