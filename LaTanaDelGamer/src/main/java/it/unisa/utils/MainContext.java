66e603b4880738cba5593e07764204a99fb80672
rbalestrieri5 <r.balestrieri5@studenti.unisa.it>
2021-06-09 10:25:59 +0200

    first edit MainContext

--- LaTanaDelGamer/src/main/java/it/unisa/utils/MainContext.java
+++ LaTanaDelGamer/src/main/java/it/unisa/utils/MainContext.java
@@ -1,5 +1,7 @@
 package it.unisa.utils;
 
+import java.sql.*;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -15,22 +17,44 @@ public class MainContext implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		Utility.print("Startup web application");
-		
+
 		ServletContext context = sce.getServletContext();
-		
+
 		DataSource ds = null;
+
 		try {
-		Context initCtx = new InitialContext();
-		Context envCtx = (Context) initCtx.lookup("java.comp/env");
-		
-		ds = (DataSource) envCtx.lookup("jdbc/la_tana_del_gamer");
-		} catch(NamingException e) {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/la_tana_del_gamer");
+
+			Connection con = null;
+			try {
+				con = ds.getConnection();
+
+				DatabaseMetaData metaData = con.getMetaData();
+				Utility.print("DBMS name: " + metaData.getDatabaseProductName());
+				Utility.print("DBMS version: " + metaData.getDatabaseProductVersion());
+
+			} finally {
+				if (con != null)
+					con.close();
+			}
+		} catch (SQLException | NamingException e) {
 			Utility.print(e);
-		}
+		} 
+
+		context.setAttribute("DataSource", ds);
+		Utility.print("DataSource creation: " + ds.toString());
+
 	}
 	
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
+		ServletContext context = sce.getServletContext();
+		
+		context.removeAttribute("DataSource");
+		
 		Utility.print("Shutdown web application");
 	}
 }
