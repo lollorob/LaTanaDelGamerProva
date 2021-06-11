abaa564b90f579205a4e4e2de7e8cd2b12d8db2c
rbalestrieri5 <r.balestrieri5@studenti.unisa.it>
2021-06-11 10:29:38 +0200

    CATEGORIA E GALLERIA MODELDS

--- LaTanaDelGamer/src/main/java/it/unisa/model/CategoriaModelDS.java
+++ LaTanaDelGamer/src/main/java/it/unisa/model/CategoriaModelDS.java
@@ -1,38 +1,198 @@
 package it.unisa.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.sql.DataSource;
+
+import it.unisa.utils.Utility;
 
 public class CategoriaModelDS implements EntityModel<CategoriaBean> {
+	private DataSource ds = null;
+	
+	public CategoriaModelDS(DataSource ds) {
+		this.ds = ds; 
+	}
 
 	@Override
 	public CategoriaBean doRetrieveByKey(Object chiave) throws SQLException {
-		// TODO Auto-generated method stub
+		String code = (String)chiave;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		CategoriaBean categoria = new CategoriaBean();
+
+		String selectSQL = "SELECT * FROM categoria WHERE nome = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setString(1, code);
+
+			Utility.print("doRetrieveByKey: " + preparedStatement.toString());
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				categoria.setNome(rs.getString("nome"));
+				categoria.setDescrizione(rs.getString("descrizione"));
+			}
+
+			Utility.print(categoria.toString());
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public Collection<CategoriaBean> doRetrieveAll(String ordine) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		String selectSQL = "SELECT * FROM categoria";
+		
+		if(ordine != null && !ordine.equals("")) {
+			selectSQL += " ORDER BY " + ordine;
+		}
+		
+		Collection<CategoriaBean> categorie = new LinkedList<CategoriaBean>();
+		
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			
+			Utility.print("doRetriveAll: " + preparedStatement.toString());
+			
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			while(rs.next()) {
+				CategoriaBean categoria = new CategoriaBean();
+				
+				categoria.setNome(rs.getString("nome"));
+				categoria.setDescrizione(rs.getString("descrizione"));
+				
+				categorie.add(categoria);
+			}
+			
+		} finally {
+			try {
+			if(preparedStatement != null)
+				preparedStatement.close();
+			}finally {
+			if(connection != null)
+				connection.close();
+			}
+		  }
+		return categorie;
 	}
 
 	@Override
 	public void doSave(CategoriaBean item) throws SQLException {
-		// TODO Auto-generated method stub
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		String insertSQL = "INSERT INTO categoria" + " (nome,descrizione) VALUES (?, ?)";
+
+		try {
+			connection = ds.getConnection();
+			connection.setAutoCommit(false);
+			preparedStatement = connection.prepareStatement(insertSQL);
+
+			preparedStatement.setString(1, item.getNome());
+			preparedStatement.setString(2, item.getDescrizione());
+				
+			Utility.print("doSave: " + preparedStatement.toString());
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public void doUpdate(CategoriaBean item) throws SQLException {
-		// TODO Auto-generated method stub
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		String updateSQL = "UPDATE categoria SET " + "descrizione = ? WHERE nome = ?";
+
+		try {
+			connection = ds.getConnection();
+			connection.setAutoCommit(false);
+			preparedStatement = connection.prepareStatement(updateSQL);
+
+			preparedStatement.setString(1, item.getDescrizione());
+
+			
+			preparedStatement.setString(2, item.getNome());
+
+			Utility.print("doUpdate: " + preparedStatement.toString());
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public void doDelete(CategoriaBean item) throws SQLException {
-		// TODO Auto-generated method stub
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
 
+		String deleteSQL = "DELETE FROM categoria WHERE nome = ?";
+
+		try {
+			connection = ds.getConnection();
+			connection.setAutoCommit(false);
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setString(1, item.getNome());
+
+			Utility.print("doDelete: " + preparedStatement.toString());
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
 	}
 
 }
--- LaTanaDelGamer/src/main/java/it/unisa/model/GalleriaModelDS.java
+++ LaTanaDelGamer/src/main/java/it/unisa/model/GalleriaModelDS.java
@@ -1,38 +1,207 @@
 package it.unisa.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.sql.DataSource;
+
+import it.unisa.utils.Utility;
 
 public class GalleriaModelDS implements EntityModel<GalleriaBean> {
 
+	private DataSource ds = null;
+	
+	public GalleriaModelDS(DataSource ds) {
+		this.ds = ds;
+	}
+	
+	
 	@Override
 	public GalleriaBean doRetrieveByKey(Object chiave) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		int code = (int)chiave;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		GalleriaBean galleria = new GalleriaBean();
+
+		String selectSQL = "SELECT * FROM galleria WHERE id_media = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setInt(1, code);
+
+			Utility.print("doRetrieveByKey: " + preparedStatement.toString());
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				galleria.setId_media(rs.getInt("id_media"));
+				galleria.setId_prodotto(rs.getInt("id_prodotto"));
+			}
+
+			Utility.print(galleria.toString());
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
+
+		return galleria;
 	}
 
 	@Override
 	public Collection<GalleriaBean> doRetrieveAll(String ordine) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		String selectSQL = "SELECT * FROM categoria";
+		
+		if(ordine != null && !ordine.equals("")) {
+			selectSQL += " ORDER BY " + ordine;
+		}
+		
+		Collection<GalleriaBean> gallerie = new LinkedList<GalleriaBean>();
+		
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			
+			Utility.print("doRetriveAll: " + preparedStatement.toString());
+			
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			while(rs.next()) {
+				GalleriaBean galleria = new GalleriaBean();
+				
+				galleria.setId_media(rs.getInt("id_media"));
+				galleria.setId_prodotto(rs.getInt("id_prodotto"));
+
+				
+				gallerie.add(galleria);
+			}
+			
+		} finally {
+			try {
+			if(preparedStatement != null)
+				preparedStatement.close();
+			}finally {
+			if(connection != null)
+				connection.close();
+			}
+		  }
+		
+		return gallerie;
+		
 	}
 
 	@Override
 	public void doSave(GalleriaBean item) throws SQLException {
-		// TODO Auto-generated method stub
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		String insertSQL = "INSERT INTO galleria" + " (id_media, id_prodotto) VALUES (?, ?)";
+
+		try {
+			connection = ds.getConnection();
+			connection.setAutoCommit(false);
+			preparedStatement = connection.prepareStatement(insertSQL);
+
+			preparedStatement.setInt(1, item.getId_media());
+			preparedStatement.setInt(2, item.getId_prodotto());
+			
+			Utility.print("doSave: " + preparedStatement.toString());
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public void doUpdate(GalleriaBean item) throws SQLException {
-		// TODO Auto-generated method stub
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		String updateSQL = "UPDATE galleria SET " + "id_prodotto = ? WHERE id_media = ?";
+
+		try {
+			connection = ds.getConnection();
+			connection.setAutoCommit(false);
+			preparedStatement = connection.prepareStatement(updateSQL);
+
+			
+			preparedStatement.setInt(1, item.getId_prodotto());
+
+			
+			preparedStatement.setInt(2, item.getId_media());
+
+			Utility.print("doUpdate: " + preparedStatement.toString());
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public void doDelete(GalleriaBean item) throws SQLException {
-		// TODO Auto-generated method stub
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
 
+		String deleteSQL = "DELETE FROM galleria WHERE id_media = ?";
+
+		try {
+			connection = ds.getConnection();
+			connection.setAutoCommit(false);
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setInt(1, item.getId_media());
+
+			Utility.print("doDelete: " + preparedStatement.toString());
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null) {
+					connection.close();
+				}
+			}
+		}
 	}
 
 }
+
+
